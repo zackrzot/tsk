8f0ea71b2457848fc129d18e81b3811e1af70016
Zachary Rzotkiewicz <zackrzot@gmail.com>
2016-10-18 12:56:21 -0400

    Update login.controller.js
    Restoring to original apiUrl format.

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -39,7 +39,7 @@ router.get('/', function (req, res) {
 router.post('/', function (req, res) {
     // authenticate using api to maintain clean separation between layers
     request.post({
-        url: config.apiUrl+':'+(process.env.PORT || config.port)+ '/api/users/authenticate',
+        url: config.apiUrl+'/users/authenticate',
         form: req.body,
         json: true
     }, function (error, response, body) {
